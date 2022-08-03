22693710356fe54d15d4a2ccf86730ffc424bdbd
dalygarrett <gdaly@yext.com>
2022-08-03 09:35:39 -0400

    url

--- src/components/offerings.tsx
+++ src/components/offerings.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 import Markdown from 'markdown-to-jsx';
 
-const offerings = (props:any) => {
+const Offerings = (props:any) => {
     const { offerings } = props;
-    const offeringsDivs = offerings.map((offerings:any) => (
+    const offeringDivs = offerings.map((offering:any) => (
       <div className="bg-gray-200 p-4 rounded-lg drop-shadow-md space-y-5">
-        <h3 className="text-lg font-semibold">{offerings.name}</h3>
-        <p><Markdown>{offerings.richTextDescription}</Markdown></p>
+        <div className="w-6/12 sm:w-4/12 px-4">
+            <img src={offering.primaryPhoto}/>
+        </div>
+        <h3 className="text-lg font-semibold">{offering.name}</h3>
+        <p><Markdown>{offering.richTextDescription}</Markdown></p>
       </div>
     ));
 
@@ -16,11 +19,11 @@ const offerings = (props:any) => {
         <div>
             <h2 className="text-xl align-center text-center font-bold mb-4">Offerings</h2>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                {offeringsDivs}
+                {offeringDivs}
             </div>
         </div>
     </>
   );
 };
 
-export default offerings;
+export default Offerings;
