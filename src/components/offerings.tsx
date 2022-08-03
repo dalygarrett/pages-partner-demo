f2e69ea200d28ed7334e2934d20131db9d448e24
dalygarrett <gdaly@yext.com>
2022-08-03 10:00:47 -0400

    changes

--- src/components/offerings.tsx
+++ src/components/offerings.tsx
@@ -3,16 +3,16 @@ import Markdown from 'markdown-to-jsx';
 
 const Offerings = (props:any) => {
     const { offerings } = props;
-    const offeringDivs = offerings.map((element:any) => (
+    const offeringDivs = offerings.map((offering:any) => (
       <div className="bg-gray-200 p-4 rounded-lg drop-shadow-md space-y-5">
         <div className="w-6/12 sm:w-4/12 px-4">
             <img
-                src={element.image.url}
+                src={offering.image}
             >
             </img>
         </div>
-        <h3 className="text-lg font-semibold">{element.name}</h3>
-        <p><Markdown>{element.richTextDescription}</Markdown></p>
+        <h3 className="text-lg font-semibold">{offering.name}</h3>
+        <p><Markdown>{offering.richTextDescription}</Markdown></p>
       </div>
     ));
 
