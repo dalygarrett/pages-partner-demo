d78f1997110164fe425c4c2ff646f6a20de9c3d4
rkeerthi <sunnykeerthi@gmail.com>
2023-01-16 19:03:10 +0530

    added email js

--- src/components/contact-us-form.tsx
+++ src/components/contact-us-form.tsx
@@ -1,65 +1,89 @@
 import * as React from "react";
-import { useState } from "react";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactUsForm = () => {
-  const [fName, setFName] = useState("");
-  const [lName, setLName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [comments, setComments] = useState("");
+  const form = useRef(null);
+  const sendEmail = (e: any) => {
+    e.preventDefault();
+    const currentForm = form.current;
+    if (currentForm == null) return;
 
+    emailjs
+      .sendForm(
+        "service_50j1947",
+        "template_s8spbem",
+        currentForm,
+        "k_atz9w6Nf-uGC1nC"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
-    <div className="bg-gray-200 p-6">
-      <div className=" ">
-        <div className="text-3xl mb-6 text-center ">Contact for a Quote</div>
+    <form ref={form} onSubmit={sendEmail}>
+      <div className="bg-gray-200 p-6">
+        <div className=" ">
+          <div className="text-3xl mb-6 text-center ">Contact for a Quote</div>
 
-        <div className="grid grid-cols-2 gap-4  m-auto">
-          <div className="col-span-2 md:col-span-1">
-            <input
-              type="text"
-              className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
-              placeholder="First Name"
-            />
-          </div>
-          <div className="col-span-2 md:col-span-1">
-            <input
-              type="text"
-              className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
-              placeholder="Last Name"
-            />
-          </div>
-          <div className="col-span-2 md:col-span-1">
-            <input
-              type="tel"
-              className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
-              placeholder="Phone number"
-            />
-          </div>
-          <div className="col-span-2 md:col-span-1">
-            <input
-              type="email"
-              className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
-              placeholder="Email Address"
-            />
-          </div>
+          <div className="grid grid-cols-2 gap-4  m-auto">
+            <div className="col-span-2 md:col-span-1">
+              <input
+                type="text"
+                className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
+                placeholder="First Name"
+                name="first_name"
+              />
+            </div>
+            <div className="col-span-2 md:col-span-1">
+              <input
+                type="text"
+                className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
+                placeholder="Last Name"
+                name="last_name"
+              />
+            </div>
+            <div className="col-span-2 md:col-span-1">
+              <input
+                type="tel"
+                className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
+                placeholder="Phone number"
+                name="phone_number"
+              />
+            </div>
+            <div className="col-span-2 md:col-span-1">
+              <input
+                type="email"
+                className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
+                placeholder="Email Address"
+                name="email"
+              />
+            </div>
 
-          <div className="col-span-2">
-            <textarea
-              cols={30}
-              rows={8}
-              className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
-              placeholder="Comments"
-            ></textarea>
-          </div>
-
-          <div className="col-span-2 text-right">
-            <button className="py-3 px-6 bg-gray-500 text-gray-200 font-bold w-full  ">
-              Submit
-            </button>
+            <div className="col-span-2">
+              <textarea
+                cols={30}
+                rows={8}
+                className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full"
+                placeholder="Comments"
+                name="message"
+              ></textarea>
+            </div>
+            <div className="col-span-2 text-right">
+              <input
+                type="submit"
+                className="py-3 px-6 bg-gray-500 text-gray-200 font-bold w-full"
+              ></input>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
